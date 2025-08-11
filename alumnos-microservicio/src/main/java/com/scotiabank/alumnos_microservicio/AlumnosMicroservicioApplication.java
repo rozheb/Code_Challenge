7cac308361034704c675e2cca26eea53cecac12b
CristopherBedon <crisaproc2000@gmail.com>
2025-08-11 18:01:07 -0500

    Carga Inicial

---
+++ alumnos-microservicio/src/main/java/com/scotiabank/alumnos_microservicio/AlumnosMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.scotiabank.alumnos_microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnosMicroservicioApplication.class, args);
+	}
+
+}
---
+++ alumnos-microservicio/src/test/java/com/scotiabank/alumnos_microservicio/AlumnosMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scotiabank.alumnos_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
